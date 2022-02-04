11a8eacf00b36bf8e947ec854981a2794ac1687e
Seungjoo Kim <sjskybone@gmail.com>
2022-02-04 00:20:19 -0800

    Trying to fixing unmounted component error. #2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { CssBaseline, Grid } from '@material-ui/core';
-import { getPlacesData } from './api';
+import { getPlacesData } from './api/index';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
@@ -8,7 +8,7 @@ import Map from './components/Map/Map';
 const App = () => {
   const [places, setPlaces] = useState([]);
 
-  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [coordinates, setCoordinates] = useState({});
   const [bounds, setBounds] = useState(null);
 
   useEffect(() => {
@@ -18,12 +18,8 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log(coordinates, bounds);
-
-    getPlacesData()
+    getPlacesData(bounds.sw, bounds.ne)
       .then((data) => {
-        console.log(data);
-
         setPlaces(data);
       })
   }, [coordinates, bounds]);
@@ -34,7 +30,7 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{width: '100%'}}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map
